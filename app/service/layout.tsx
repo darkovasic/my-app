df197461f4e1fd536815458a0613ce166d0bacb4
Darko Vasic <darko.vasic@gmail.com>
2023-11-25 10:55:22 +0100

    CRUD UI (non-hydrated)

--- app/service/layout.tsx
+++ app/service/layout.tsx
@@ -1,13 +1,9 @@
-import { ReactNode } from "react";
 import ServiceSidebar from "./_components/ServiceSidebar";
 
-function ServiceLayout({ children }: { children: ReactNode }) {
+function ServiceLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-1">
-      <ServiceSidebar />
-      <section className="flex flex-1 items-center justify-center">
-        {children}
-      </section>
+      <ServiceSidebar /> {children}
     </div>
   );
 }
