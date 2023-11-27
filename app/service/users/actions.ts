46a373c19719a1535d67be58f27e3b497ab165d0
Darko Vasic <darko.vasic@gmail.com>
2023-11-27 01:06:56 +0100

    Save user data to firebase

--- app/service/users/actions.ts
+++ app/service/users/actions.ts
@@ -1,10 +1,24 @@
 "use server";
 
+import { db } from "@/app/firebase";
+import { collection, addDoc } from "firebase/firestore";
+
 type FormState = {
   message: string;
 };
 
 export async function createUser(prevState: FormState, formData: FormData) {
   console.log("[createUser] formData", formData);
-  return { message: "Failed to create user" };
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      email: formData.get("email"),
+    });
+    console.log("Document written with ID: ", docRef.id);
+    return { message: "User is created." };
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    return { message: "Error adding document." };
+  }
 }
--- components/crud/modals/CreateUserModal.tsx
+++ components/crud/modals/CreateUserModal.tsx
@@ -103,15 +103,15 @@ function CreateUserModal() {
               <div className="grid gap-4 mb-4 sm:grid-cols-2">
                 <div>
                   <label
-                    htmlFor="first_name"
+                    htmlFor="firstName"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     First Name
                   </label>
                   <input
                     type="text"
-                    name="first_name"
-                    id="first_name"
+                    name="firstName"
+                    id="firstName"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder=""
                     required
@@ -119,15 +119,15 @@ function CreateUserModal() {
                 </div>
                 <div>
                   <label
-                    htmlFor="last_name"
+                    htmlFor="lastName"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
                     Last Name
                   </label>
                   <input
                     type="text"
-                    name="last_name"
-                    id="last_name"
+                    name="lastName"
+                    id="lastName"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder=""
                     required
