345ced5c028179b0c53252ff69d455e2bd16f8ab
Darko Vasic <darko.vasic@gmail.com>
2023-12-21 16:01:18 +0100

    User table refactoring

--- app/service/users/page.tsx
+++ app/service/users/page.tsx
@@ -1,6 +1,6 @@
 import CrudHeader from "@/components/crud/CrudHeader";
 import Pagination from "@/components/crud/Pagination";
-import UserDropdownMenu from "./_components/UserDropdownMenu";
+import UserTable from "./_components/UserTable";
 import { getFirstUsers, getNextUsers } from "./actions";
 import type { Metadata } from "next";
 
@@ -30,55 +30,7 @@ const UsersPage = async ({ searchParams }: UsersPageProps) => {
         <div className="bg-white h-full dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden">
           <div className="overflow-x-auto h-full">
             <CrudHeader />
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="px-4 py-4">
-                    Name
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    Email
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    Role
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    Created At
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    Updated At
-                  </th>
-                  <th scope="col" className="px-4 py-3">
-                    <span className="sr-only">Actions</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user) => {
-                  return (
-                    <tr key={user.id} className="border-b dark:border-gray-700">
-                      <th
-                        scope="row"
-                        className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        {`${user?.firstName} ${user?.lastName}`}
-                      </th>
-                      <td className="px-4 py-3">{user?.email}</td>
-                      <td className="px-4 py-3">{user?.role}</td>
-                      <td className="px-4 py-3">
-                        {user?.createdAt?.toString()}
-                      </td>
-                      <td className="px-4 py-3">
-                        {user?.updatedAt?.toString()}
-                      </td>
-                      <td className="px-4 py-3 flex items-center justify-end">
-                        <UserDropdownMenu user={user} />
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+            <UserTable users={users} />
             <Pagination currentPage={currentPage} totalPages={totalPages} />
           </div>
         </div>
